edb2df12d5e697479d89972c31abc37fc59333f7
Khvostenko <khmark04@mail.ru>
2022-01-18 18:14:56 +0700

    initial commit

---
+++ src/test/java/HttpRequestTest.java
@@ -0,0 +1,22 @@
+import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class HttpRequestTest {
+
+    @Test
+    public void shouldReturnSomeData() {
+
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"));
+    }
+
+}
